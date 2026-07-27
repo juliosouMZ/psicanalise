da2122337ecb0d478f9906ad3001e4da9be30642
juliosouMZ <113320465+juliosouMZ@users.noreply.github.com>
2026-07-27 09:39:10 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// =============================
+// HEADER
+// =============================
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>50){
+
+header.classList.add("scrolled");
+
+}else{
+
+header.classList.remove("scrolled");
+
+}
+
+});document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+const target=document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});const faq=document.querySelectorAll(".faq-item");
+
+faq.forEach(item=>{
+
+item.addEventListener("click",()=>{
+
+item.classList.toggle("active");
+
+});
+
+});
